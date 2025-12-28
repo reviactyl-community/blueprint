0be560c6626a053941f8a20116756d12f313f8c7
BijjuXD <103484184+BijjuXD@users.noreply.github.com>
2025-12-28 14:39:55 +0530

    style: update ui for schedule

--- resources/scripts/components/server/schedules/ScheduleEditContainer.tsx
+++ resources/scripts/components/server/schedules/ScheduleEditContainer.tsx
@@ -26,7 +26,7 @@ interface Params {
 }
 
 const CronBox = ({ title, value }: { title: string; value: string }) => (
-    <div css={tw`bg-neutral-700 rounded p-3`}>
+    <div css={tw`bg-gray-700 rounded-ui p-3`}>
         <p css={tw`text-neutral-300 text-sm`}>{title}</p>
         <p css={tw`text-xl font-medium text-neutral-100`}>{value}</p>
     </div>
@@ -87,12 +87,10 @@ export default () => {
                 <Spinner size={'large'} centered />
             ) : (
                 <>
-                    <BeforeEdit />
+                <BeforeEdit />
                     <ScheduleCronRow cron={schedule.cron} css={tw`sm:hidden bg-neutral-700 rounded mb-4 p-3`} />
-                    <div css={tw`rounded shadow`}>
-                        <div
-                            css={tw`sm:flex items-center bg-neutral-900 p-3 sm:p-6 border-b-4 border-neutral-600 rounded-t`}
-                        >
+                    <div css={tw`rounded-ui shadow bg-gray-700 border border-gray-600`}>
+                        <div css={tw`sm:flex items-center p-3 sm:p-6 border-b-4 border-neutral-600 rounded-t`}>
                             <div css={tw`flex-1`}>
                                 <h3 css={tw`flex items-center text-neutral-100 text-2xl`}>
                                     {schedule.name}
@@ -170,7 +168,7 @@ export default () => {
                             </Can>
                         )}
                     </div>
-                    <AfterEdit />
+                <AfterEdit />
                 </>
             )}
         </PageContentBlock>
