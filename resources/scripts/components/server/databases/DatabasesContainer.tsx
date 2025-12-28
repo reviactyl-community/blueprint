c5f9773bec666d055d8782882880355b2e332394
BijjuXD <103484184+BijjuXD@users.noreply.github.com>
2025-12-28 14:39:09 +0530

    style: update ui for databases

--- resources/scripts/components/server/databases/DatabasesContainer.tsx
+++ resources/scripts/components/server/databases/DatabasesContainer.tsx
@@ -12,11 +12,15 @@ import tw from 'twin.macro';
 import Fade from '@/components/elements/Fade';
 import ServerContentBlock from '@/components/elements/ServerContentBlock';
 import { useDeepMemoize } from '@/plugins/useDeepMemoize';
+import Card from '@/reviactyl/ui/Card';
+import { DatabaseIcon } from '@heroicons/react/solid';
+import { useTranslation } from 'react-i18next';
 
 import BeforeContent from '@blueprint/components/Server/Databases/BeforeContent';
 import AfterContent from '@blueprint/components/Server/Databases/AfterContent';
 
 export default () => {
+    const { t } = useTranslation('server/databases');
     const uuid = ServerContext.useStoreState((state) => state.server.data!.uuid);
     const databaseLimit = ServerContext.useStoreState((state) => state.server.data!.featureLimits.databases);
 
@@ -40,14 +44,14 @@ export default () => {
     }, []);
 
     return (
-        <ServerContentBlock title={'Databases'}>
+        <ServerContentBlock title={t('title')}>
             <FlashMessageRender byKey={'databases'} css={tw`mb-4`} />
             {!databases.length && loading ? (
                 <Spinner size={'large'} centered />
             ) : (
                 <Fade timeout={150}>
                     <>
-                        <BeforeContent />
+                    <BeforeContent />
                         {databases.length > 0 ? (
                             databases.map((database, index) => (
                                 <DatabaseRow
@@ -57,18 +61,18 @@ export default () => {
                                 />
                             ))
                         ) : (
-                            <p css={tw`text-center text-sm text-neutral-300`}>
-                                {databaseLimit > 0
-                                    ? 'It looks like you have no databases.'
-                                    : 'Databases cannot be created for this server.'}
-                            </p>
+                            <Card>
+                                <p css={tw`flex justify-center text-center text-sm text-gray-400`}>
+                                    <DatabaseIcon className='w-5 h-5 mr-1' />
+                                    {databaseLimit > 0 ? t('out-of-databases') : t('no-databases')}
+                                </p>
+                            </Card>
                         )}
                         <Can action={'database.create'}>
                             <div css={tw`mt-6 flex items-center justify-end`}>
                                 {databaseLimit > 0 && databases.length > 0 && (
                                     <p css={tw`text-sm text-neutral-300 mb-4 sm:mr-6 sm:mb-0`}>
-                                        {databases.length} of {databaseLimit} databases have been allocated to this
-                                        server.
+                                        {t('created', { count: databases.length, limit: databaseLimit })}
                                     </p>
                                 )}
                                 {databaseLimit > 0 && databaseLimit !== databases.length && (
@@ -76,7 +80,7 @@ export default () => {
                                 )}
                             </div>
                         </Can>
-                        <AfterContent />
+                    <AfterContent />
                     </>
                 </Fade>
             )}
